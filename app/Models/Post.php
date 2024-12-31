22feed2adcceed6cf631af770175e849fead1293
Edidiong <eddyenin6@gmail.com>
2024-12-31 09:25:33 +0100

    Modified the post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,25 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    protected $fillable = [
-        'category_id',
-        'title',
-        'slug',
-        'image',
-        'body'
-    ];
+    use HasFactory;
 
-    /**
-     * Get the user that owns the Post
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function category(): BelongsTo
+    public function category():BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
@@ -29,4 +19,6 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+
 }
